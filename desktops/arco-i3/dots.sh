f962e7426ba2bb57bcc48cf683a55087d2f08a8c
dwhite944 <david.w.white@gmail.com>
2019-10-09 16:03:51 -0500

    Comment :  on Wed 09 Oct 2019 04:03:51 PM CDT

--- desktops/arco-i3/dots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone --recursive --separate-git-dir=$HOME/.dots.git https://github.com/amanre/dots.git /tmp/dots
-rsync -rvl --exclude ".git" /tmp/dots/ $HOME/
-rm -r /tmp/dots
-sudo rm -r ~/.dots.git
-sudo rm -r ~/.git
-#sudo rm -r ~/desktops
